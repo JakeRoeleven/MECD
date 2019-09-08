6104a2640cb87e9ae8d6df2b18a7b5caf7b04109
Jake <jakeroeleven44@gmail.com>
2019-09-08 21:29:48 +1000

    Added libs

---
+++ libs/Compression/compression.c
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * 48430 Fundamentals of C Programming - Group Assignment
+ * Authors:
+ * 	Jake Roeleven
+ * Date Complete:
+*******************************************************************************/
+
+int main(void) {
+	return 0;
+}
