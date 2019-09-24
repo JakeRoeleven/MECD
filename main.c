62f817aba86fd0541586a2ec7a599bb64251bb66
Beichen-Man <55418281+Beichen-Man@users.noreply.github.com>
2019-09-24 13:51:06 +1000

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
  * Date Complete:
 *******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 int main(void) {
 	return 0;
 }
@@ -14,26 +19,33 @@ void menu() {
 	int quit=0;
         while (!quit) {
 		printf ("\n");
-		printf ("addfile\n");
-		printf ("display recently encrypted and compressed files")
-		printf ("
+		printf ("Search\n");
+		printf ("Creat Entry\n");
+		printf ("display recently encrypted and compressed files\n");
+		printf ("Save Entries to Database\n");
+		printf ("Load Database\n");
+		printf ("Encrypt Database\n");
+		printf ("Compress Database\n");
 			
 		printf (""Enter choice (number between 1-5)>\n");
 		scanf ("&d", choice);
 		
 		switch (choice){
-		      case 1:
-			addfile ();
+	              case 1:
+			Search ();
 			break;
 		      case 2:
-			displayfiles ();
+			CreatEntry ();
+			break;
+		      case 3:
+			Displayfiles ();
 			break;
 			
 			
 	return 0;
 }
 
-void addfile(){
+void CreatEntry(){
 	file_t new_file;
 	char buffer[1000]
 	
