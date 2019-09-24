c3ab67485fadd1d60ed5b22b8a6d1c06d58bea3f
Beichen-Man <55418281+Beichen-Man@users.noreply.github.com>
2019-09-24 13:00:46 +1000

    Update main.c

--- main.c
+++ main.c
@@ -8,3 +8,33 @@
 int main(void) {
 	return 0;
 }
+
+void menu() {
+	int choice;
+	int quit=0;
+        while (!quit) {
+		printf ("\n");
+		printf ("addfile\n");
+		printf ("display recently encrypted and compressed files")
+		printf ("
+			
+		printf (""Enter choice (number between 1-5)>\n");
+		scanf ("&d", choice);
+		
+		switch (choice){
+		      case 1:
+			addfile ();
+			break;
+		      case 2:
+			displayfiles ();
+			break;
+			
+			
+	return 0;
+}
+
+void addfile(){
+	file_t new_file;
+	char buffer[1000]
+	
+	
