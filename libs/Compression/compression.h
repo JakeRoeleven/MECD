7ecd75207b83f056d69d53fb4394d99d8f0f4716
Jake Roeleven <48663153+JakeRoeleven@users.noreply.github.com>
2019-09-29 22:52:46 +1000

    Compression (#2)
    * Begin Compression
    
    * Huffman Tree should now build
    
    * Compression works!
    
    * We have liftoff
    
    * Added error checks, gitignore and file choice
    
    * cleaned files

--- libs/Compression/compression.h
+++ libs/Compression/compression.h
@@ -5,6 +5,48 @@
  * Date Complete:
 *******************************************************************************/
 
-int main(void) {
-	return 0;
-}
+#ifndef COMPRESSION_H
+#define COMPRESSION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CHAR  257   
+#define EOF_FLAG    256    
+#define CHAR_BIT_SIZE   8
+
+/*Structs*/
+typedef struct HuffmanNode Node; 
+
+struct HuffmanNode {
+    int contents;
+    int frequency;
+    Node *leftP, *rightP;
+};
+
+Node *buildHuffmanTree(int frequenciesArray[]);
+int compareNodes(const void *nodeOneP, const void *nodeTwo);
+char **buildCharacterTable( int frequenciesArray[]);
+
+void searchHuffmanTree(Node *treeP, char **tableP, char *toFindP);
+char *addPrefix(char *current, char prefix);
+
+void writeFileHeader(FILE *outFile, int frequenciesArray[]);
+int *readFileHeader(FILE *inFile);
+
+void encodeBit(const char *bitArrayP, FILE *outFileP);
+int decodeBit(FILE *inFileP);
+int bitToChar(FILE *inFileP, Node *treeP);
+void freeHuffmanTree(Node *treeP);
+void freeTable(char *tableP[]);
+void encodeFile(FILE *in, FILE *out);
+void decodeFile(FILE *in, FILE *out);
+
+void Compress();
+void Decompress();
+
+char* removeFileExtension(char* str);
+
+
+#endif
