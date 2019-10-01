509231a7925f7ea4487d7bb754b73815cbef024c
Jake <jakeroeleven44@gmail.com>
2019-10-01 12:15:40 +1000

    Set up structs

---
+++ libs/Records/records.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * 48430 Fundamentals of C Programming - Group Assignment
+ * 
+ * Records 
+ * 
+ * Author(s):
+ * 	Jake Roeleven - 13246638
+ * Date Complete:
+ * 
+ * Notes: 
+*******************************************************************************/
+
+#ifndef RECORD
+#define RECORD
+
+/* Inclusions */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct MilitaryRecord Record;
+typedef struct Date Date;
+
+struct Date {
+	int day;
+	int month;
+	int year;
+}
+
+struct MilitaryRecord {
+	char *rank, *fNameP, *lNameP;
+	int *militaryID[9];
+	Date DOB; 
+	int yearsInService;
+	char *division;
+	int currentlyActive;
+	char* currentDeployment;
+};
+
+#endif
