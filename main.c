d22ad44d5bba4f5716c87049d6426df6828bbccc
Jake <jakeroeleven44@gmail.com>
2019-10-03 23:52:05 +1000

    Command Line Parsing Implemented

--- main.c
+++ main.c
@@ -8,26 +8,112 @@
  * Date Complete:
 *******************************************************************************/
 
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "libs/Interface/interface.c"
 
-void menu();
+#define Green "\e[32m"
+#define printf(X) printf(Green "%s",X);
 
-int main() {
-	menu();
+void utilityMenu();
+void helpMenu();
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+
+    printf("\n\n");
+
+	if (argc > 1) {
+        for (i = 1; i < argc; i++) {
+            switch(*argv[i]) {  
+                case 'h': 
+            		helpMenu();
+            		break;
+                case 'c': 
+            		printf("Create Records: TODO");
+            		break;
+                case 'v': 
+            		printf("View Records: TODO");
+            		break;
+                case 'e': 
+                    printf("Encrypt and Compress: TODO");
+            		break;
+                case 'd': 
+                    printf("Decrypt and Decompress: TODO");
+            		break;
+                case 'u': 
+                    utilityMenu();
+            		break;
+                case 'r': 
+                    printf("Recent Files: TODO");
+            		break;
+                default:
+                    printf("Sorry that's not a valid argument!\n");
+                    printf("The following arguments are available: \n"
+                    	   "-----------------------------------------------------------------\n"
+                    	   "h - View the Help Menu\n"
+                    	   "c - Create and Save Military Records\n"
+                    	   "v - View Military Records\n"
+                    	   "e - Encrypt and Compress Records\n"
+                    	   "d - Decompress and Decrypt Records\n"
+                    	   "u - View the Utility Menu (access the Help Menu for more info...)\n"
+                    	   "r - View Recent Files\n"
+                    	   "-----------------------------------------------------------------\n");
+                    return 0;
+            } 
+        }
+    } 
+    else {
+        printf("Hey! You didn't pass in an argument! Try Again!\n");
+        return 0;
+    }
+
+    printf("\n\n");
+    /*Saftey Return*/
 	return 0;
+}
+
+void helpMenu() {
+    
+    printf("Welcome to the MECD Help Menu!\n\n");
+    printf("The following arguments are available to be passed in: \n"
+    	   "-----------------------------------------------------------------\n"
+    	   "h - View this Help Menu\n"
+    	   "c - Create and Save Military Records\n"
+    	   "    > This option allows you to create new military records.\n"
+    	   "    > You can choose to save the records in to a text file through\n"
+    	   "      this option.\n"
+    	   "v - View Military Records\n"
+    	   "    > This option allows you to view properly formatted military\n"
+    	   "      records.\n"
+    	   /* TODO: Confirm this */
+    	   "    > You can search and sort records through this option!\n"
+    	   "e - Encrypt and Compress Records\n"
+    	   "    > This option encrypts and compresses you files.\n"
+    	   "    > Your file will be avalible as a .min file after this option.\n"
+    	   "d - Decompress and Decrypt Records\n"
+    	   "    > This option decompress and decrypt you files.\n"
+    	   "    > Your file will be avalible as a .txt file after this option.\n"
+    	   "u - View the Utility Menu (access the Help Menu for more info...)\n"
+    	   "    > This option allows you to access a utility menu that allows you to\n"
+    	   "      choose wether to compress, decompress, encrypt or decrypt files.\n"
+    	   "r - View Recent Files\n"
+    	   "    > View the files you recently interacted with.\n"
+    	   "-----------------------------------------------------------------\n");
 }
 
 
 
-void menu() {
+void utilityMenu() {
+    printf("Welcome to the Utilities Menu!\n\n");
 	int choice;
 	int quit=0;
         while (!quit) {
-			/*TODO: Menu function */
 			printf("\n");
 			printf("Enter Choice:\n");
 			printf("1 - Compress File\n");
@@ -54,7 +140,7 @@ void menu() {
 				decryptOnly();
 				break;
 			default:
-				printf("Invalid choice\n");
+				printf("Hey! That's an Invalid Choice! Try Again!\n");
 				break;
 		}
 	}
